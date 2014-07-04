f87d2d2b921383ef17edbeeb24e0d5f58b357a3b
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-07-04 10:59:44 +0200

    move more code into wpsui

--- wpsui.js
+++ wpsui.js
@@ -25,3 +25,52 @@ wps.client.prototype.getGroupedProcesses = function(callback) {
       callback.call(this, groups, info);
     });
 };
+
+wps.ui = function(options) {
+  this.parentContainer_ = options.parentContainer;
+  this.sideBar_ = options.sideBar;
+};
+
+wps.ui.prototype.createProcessCategory = function(group) {
+  var category = $('<div class="palette-category"><div class="palette-header">' +
+    '<i class="glyphicon glyphicon-chevron-down expanded"></i><span>' +
+    group + '</span></div></div>');
+  this.parentContainer_.append(category);
+  var content = $('<div class="palette-content"></div>');
+  $(category).append(content);
+  $(".palette-header").click(function(e) {
+    $(this).next().slideToggle();
+    $(this).children("i").toggleClass("expanded");
+  });
+  return content;
+};
+
+wps.ui.prototype.createProcess = function(offering) {
+  var summary = offering['abstract'];
+  var title = offering.title;
+  var id = offering.identifier;
+  var d = $('<div class="palette_node ui-draggable">' + id.split(':')[1] + '</div>');
+  $(d).data('type', id);
+  $(d).popover({
+    title: title,
+    placement:"right",
+    trigger: "hover",
+    delay: { show: 750, hide: 50 },
+    html: true,
+    container:'body',
+    content: summary
+  });
+  var sidebar = this.sideBar_;
+  $(d).click(summary, function(evt) {
+    var help = '<div class="node-help">' + evt.data + "</div>";
+    sidebar.html(help);
+  });
+  $(d).draggable({
+    helper: 'clone',
+    appendTo: 'body',
+    revert: true,
+    revertDuration: 50
+  });
+  return d;
+};
+
