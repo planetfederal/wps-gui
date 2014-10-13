0f892175ba9826feb3c56680bc60a3ecb76920ee
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-10-13 15:09:02 +0200

    partially combine css and minify (closes #118)

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,29 @@ var gulp = require('gulp'),
     gp_rename = require('gulp-rename'),
     gp_bower_files = require('main-bower-files'),
     gp_filter = require('gulp-filter'),
+    gp_minify_css = require('gulp-minify-css'),
     gp_uglify = require('gulp-uglify');
 
+gulp.task('cssbuild', function() {
+    gulp.src(['*.png'])
+      .pipe(gulp.dest('dist'));
+    gulp.src(['vendor/ol.css', 'style.css'])
+      .pipe(gp_filter('**/*.css'))
+      .pipe(gp_concat('wps-gui.css'))
+      .pipe(gulp.dest('dist'))
+      .pipe(gp_rename('wps-gui.min.css'))
+      .pipe(gp_minify_css())
+      .pipe(gulp.dest('dist'));
+});
+
 gulp.task('jsbuild', function(){
-    return gulp.src(gp_bower_files().concat(['mappings/*.js', 'wpsclient.js', 'wpsui.js']))
+    gulp.src(gp_bower_files().concat(['mappings/*.js', 'wpsclient.js', 'wpsui.js']))
         .pipe(gp_filter('**/*.js'))
-        .pipe(gp_concat('wps-gui-debug.js'))
+        .pipe(gp_concat('wps-gui.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(gp_rename('wps-gui.js'))
+        .pipe(gp_rename('wps-gui.min.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['jsbuild'], function(){});
+gulp.task('default', ['jsbuild', 'cssbuild'], function(){});
